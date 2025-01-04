6868714d1f06e6be1f9f05856b191f8f1b9da2b8
Patrick Koch <patkoch87@gmail.com>
2025-01-04 17:44:52 +0100

    Add storage account as backend

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,13 @@ terraform {
       version = ">= 4.1"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name   = "azureworkshop-demo-rg"
+    storage_account_name  = "azureworkshopdemostorage"
+    container_name        = "tfstateopenai"
+    key                   = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
