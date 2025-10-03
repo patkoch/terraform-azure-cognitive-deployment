5fc1858668604a1165a8053d89919541f5032234
Patrick Koch <patkoch87@gmail.com>
2025-10-03 14:34:16 +0200

    Simulate a validation issue

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
-resource "azurerm_cognitive_account" "ca" {
+resource "azurerm_cognitive_account" "cax" {
   name                = var.cognitive_account_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
