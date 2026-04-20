ff251b2d0e419fc5607ccaf9342df1a306f3169b
Patrick Koch <patkoch87@gmail.com>
2026-04-20 14:39:40 +0200

    Add a bug

--- variables.tf
+++ variables.tf
@@ -56,7 +56,7 @@ variable "cognitive_account_tags" {
   }
 }
 
-variable "cognitive_deployment_name" {
+variable "cognitive_deployment" {
   type        = string
   description = "Name of the cognitive deployment"
 }
